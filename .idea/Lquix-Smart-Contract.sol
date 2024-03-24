bfea0a50c63db6a98798398066b4baf890428e87
DaliaKlinzing <klinzingdalia0@gmail.com>
2024-03-24 15:22:16 +0100

    Update

--- .idea/Lquix-Smart-Contract.sol
+++ .idea/Lquix-Smart-Contract.sol
@@ -1,13 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="true">
-    <exclude-output />
-    <content url="file://$MODULE_DIR$" />
-    <orderEntry type="inheritedJdk" />
-    <orderEntry type="sourceFolder" forTests="false" />
-  </component>
-</module>
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
